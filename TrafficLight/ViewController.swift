0891d3689673d6c6768e491e6ac6a419fddb5ba7
Dmitry Polyakov <dmitryypolyakov@gmail.com>
2022-10-31 01:07:06 +0500

    added stack for traffic light added start/next button added some logic

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redTrafficLight: UIView!
+    @IBOutlet var yellowTrafficLight: UIView!
+    @IBOutlet var greenTrafficLight: UIView!
+    @IBOutlet var startNextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redTrafficLight.layer.cornerRadius = 75
+        redTrafficLight.alpha = 0.3
+        
+        yellowTrafficLight.layer.cornerRadius = 75
+        yellowTrafficLight.alpha = 0.3
+        
+        greenTrafficLight.layer.cornerRadius = 75
+        greenTrafficLight.alpha = 0.3
+        
+        startNextButton.layer.cornerRadius = 27.5
     }
-
-
+    
+    
+    @IBAction func startButtonDidTapped() {
+        startNextButton.setTitle("NEXT", for: .normal)
+        if redTrafficLight.alpha == 1 {
+            yellowTrafficLight.alpha = 1
+            redTrafficLight.alpha = 0.3
+        } else if yellowTrafficLight.alpha == 1 {
+            greenTrafficLight.alpha = 1
+            yellowTrafficLight.alpha = 0.3
+        } else if greenTrafficLight.alpha == 1{
+            redTrafficLight.alpha = 1
+            greenTrafficLight.alpha = 0.3
+        } else {
+            redTrafficLight.alpha = 1
+        }
+    }
 }
 
