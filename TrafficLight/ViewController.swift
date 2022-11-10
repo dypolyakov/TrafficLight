ee2b8ba47ab41d2ab2157ae5f39438dac54781b4
Dmitry Polyakov <dmitryypolyakov@gmail.com>
2022-11-10 14:40:52 +0500

    Added an enumeration of the current traffic light. Replaced if statement with switch.

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -14,20 +14,25 @@ class ViewController: UIViewController {
     @IBOutlet var greenLightView: UIView!
     @IBOutlet var startNextButton: UIButton!
     
-    private let switchedOnLight: CGFloat = 1
-    private let switchedOffLight = 0.3
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff = 0.3
+    private var currentLight = CurrentLight.red
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        redLightView.alpha = lightIsOff
+        yellowLightView.alpha = lightIsOff
+        greenLightView.alpha = lightIsOff
+        
         redLightView.layer.cornerRadius = redLightView.frame.width / 2
-        redLightView.alpha = switchedOffLight
+        redLightView.alpha = lightIsOff
         
         yellowLightView.layer.cornerRadius = yellowLightView.frame.width / 2
-        yellowLightView.alpha = switchedOffLight
+        yellowLightView.alpha = lightIsOff
         
         greenLightView.layer.cornerRadius = greenLightView.frame.width / 2
-        greenLightView.alpha = switchedOffLight
+        greenLightView.alpha = lightIsOff
         
         startNextButton.layer.cornerRadius = startNextButton.frame.height / 2
     }
@@ -37,18 +42,39 @@ class ViewController: UIViewController {
             startNextButton.setTitle("NEXT", for: .normal)
         }
     
-        if redLightView.alpha == switchedOnLight {
-            yellowLightView.alpha = switchedOnLight
-            redLightView.alpha = switchedOffLight
-        } else if yellowLightView.alpha == switchedOnLight {
-            greenLightView.alpha = switchedOnLight
-            yellowLightView.alpha = switchedOffLight
-        } else if greenLightView.alpha == switchedOnLight {
-            redLightView.alpha = switchedOnLight
-            greenLightView.alpha = switchedOffLight
-        } else {
-            redLightView.alpha = switchedOnLight
+        switch currentLight {
+        case .red:
+            greenLightView.alpha = lightIsOff
+            redLightView.alpha = lightIsOn
+            currentLight = .yellow
+        case .yellow:
+            redLightView.alpha = lightIsOff
+            yellowLightView.alpha = lightIsOn
+            currentLight = .green
+        case .green:
+            yellowLightView.alpha = lightIsOff
+            greenLightView.alpha = lightIsOn
+            currentLight = .red
         }
+        
+//        if redLightView.alpha == switchedOnLight {
+//            yellowLightView.alpha = switchedOnLight
+//            redLightView.alpha = switchedOffLight
+//        } else if yellowLightView.alpha == switchedOnLight {
+//            greenLightView.alpha = switchedOnLight
+//            yellowLightView.alpha = switchedOffLight
+//        } else if greenLightView.alpha == switchedOnLight {
+//            redLightView.alpha = switchedOnLight
+//            greenLightView.alpha = switchedOffLight
+//        } else {
+//            redLightView.alpha = switchedOnLight
+//        }
     }
 }
 
+// MARK: - CurrentLight
+extension ViewController {
+    private enum CurrentLight {
+        case red, yellow, green
+    }
+}
